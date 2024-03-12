cc77f2407a4c366a03270118a5b3598f6b850e4a
dtrznadel <trznadel.dom@gmail.com>
2024-03-12 21:15:29 +0100

    add test file

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import logging
 import threading
 import tkinter as tk
 
-# from translator_app_with_memory import stream, p
-# from translator_app_with_memory.TranslatorApp import TranscriptionApp
-# from translator_app_with_memory.utils import write_audio, record_audio, translate_with_whisper, guard_with_llm
-
 from translator_app import stream, p
 from translator_app.TranslatorApp import TranscriptionApp
 from translator_app.utils import (
@@ -15,40 +11,6 @@ from translator_app.utils import (
     transcript,
     translate,
 )
-
-#
-# if __name__ == '__main__':
-#     root = tk.Tk()
-#     app = TranscriptionApp(root)
-#
-#     recording_thread = threading.Thread(target=record_audio)
-#     translate_thread = threading.Thread(target=translate_with_whisper)
-#     guard_thread = threading.Thread(target=guard_with_llm)
-#     writing_thread = threading.Thread(target=write_audio)
-#
-#
-#     recording_thread.start()
-#     translate_thread.start()
-#     guard_thread.start()
-#     writing_thread.start()
-#
-#     root.mainloop()
-#
-#     recording_thread.join()
-#     translate_thread.join()
-#     guard_thread.join()
-#     writing_thread.join()
-#
-#
-#     stream.stop_stream()
-#     stream.close()
-#     p.terminate()
-# #
-# #
-
-# from translator_app_test import stream, p
-# from translator_app_test.TranslatorApp import TranscriptionApp
-# from translator_app_test.utils import write_audio, record_audio
 
 if __name__ == "__main__":
     root = tk.Tk()
