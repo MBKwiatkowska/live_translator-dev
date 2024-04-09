650aa9d652b336271b97a630a74be552a4cda75c
MBKwiatkowska <merida.reports@gmail.com>
2024-04-09 19:11:15 +0200

    komentarz

--- translator_app/utils.py
+++ translator_app/utils.py
@@ -232,6 +232,7 @@ def translate(
                 },
             ]
 
+################## translate with openai
             response = client.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=prepared_input,
